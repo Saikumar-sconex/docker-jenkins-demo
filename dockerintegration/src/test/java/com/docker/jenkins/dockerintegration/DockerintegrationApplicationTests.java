436c9096ab796887aec34dbfafb01b5189da9cda
Saikumar-sconex <saikumar.medapati@sconexsoft.com>
2026-06-18 22:26:20 +0530

    first commit

---
+++ dockerintegration/src/test/java/com/docker/jenkins/dockerintegration/DockerintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenkins.dockerintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
